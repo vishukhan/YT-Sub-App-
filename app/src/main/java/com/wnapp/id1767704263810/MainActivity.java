9e29ea3f055aaf51a869f3301063ac4718ea299f
Vishal <vk8976914@gmail.com>
2026-01-14 12:23:56 +0000

    Fix: Setup GitHub Actions Build

---
+++ app/src/main/java/com/wnapp/id1767704263810/MainActivity.java
@@ -0,0 +1,12 @@
+<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.wnapp.id1767704263810">
+    <supports-screens android:smallScreens="true" android:normalScreens="true" android:largeScreens="true" android:xlargeScreens="true" android:anyDensity="true" />
+    <uses-permission android:name="android.permission.INTERNET" />
+    <application android:label="YT-Sub-App">
+        <activity android:name=".MainActivity" android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+    </application>
+</manifest>
