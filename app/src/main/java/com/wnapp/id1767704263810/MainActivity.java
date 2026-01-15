9078e3bc4641fbc0d69fee58f3b02c7dd483b0a2
Vishal <vk8976914@gmail.com>
2026-01-15 03:12:45 +0000

    Final Fix: MainActivity and Resources

--- app/src/main/java/com/wnapp/id1767704263810/MainActivity.java
+++ app/src/main/java/com/wnapp/id1767704263810/MainActivity.java
@@ -1,12 +1,9 @@
-<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.wnapp.id1767704263810">
-    <supports-screens android:smallScreens="true" android:normalScreens="true" android:largeScreens="true" android:xlargeScreens="true" android:anyDensity="true" />
-    <uses-permission android:name="android.permission.INTERNET" />
-    <application android:label="YT-Sub-App">
-        <activity android:name=".MainActivity" android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
-                <category android:name="android.intent.category.LAUNCHER" />
-            </intent-filter>
-        </activity>
-    </application>
-</manifest>
+package com.wnapp.id1767704263810;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
