7768630ca6621594768ce4c51a8720b4b24b1490
Vishal <vk8976914@gmail.com>
2026-01-31 19:18:22 +0530

    sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Ye empty fetch handler zaroori hai PWA installable banane ke liye
-});
-
